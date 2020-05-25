394c397f20ab0326e4c7afd7d465c247e86ab3f6
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-05-24 18:49:20 -0700

    add todo functionality

--- App.js
+++ App.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const inputHandler = text => {
+    setInput(text)
+  }
+
+  const addTodo = () => {
+    setTodos(currentState => [...currentState, input])
+  }
+
   return (
-    <View style={styles.container}>
+    <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder='to do...' style={styles.input} />
-        <Button title='add' />
+        <TextInput placeholder='to do...' style={styles.input} onChangeText={inputHandler} />
+        <Button title='add' onPress={addTodo} />
       </View>
       <View>
-
+        {todos.map((e, i) => <Text key={i}>{e}</Text>)}
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     padding: 70,
     backgroundColor: '#faf74d',
     height: '100%',
